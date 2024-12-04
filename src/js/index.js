ef4b4a38e1729200fa1b25561e153f8173336855
andreinapazz <108377491+andreinapazz@users.noreply.github.com>
2024-12-04 07:30:39 +0000

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,42 @@
 //import react into the bundle
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React, { useDeferredValue, useEffect, useState } from "react";
+import ReactDOM from 'react-dom/client';
 
-// include your styles into the webpack bundle
 import "../styles/index.css";
-
-//import your own components
+import './icons.js'
 import Home from "./component/home.jsx";
+
+
+
+function App () {
+    
+    const [counter, setCounter] = useState (0);
+    
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCounter(counter => +1) 
+        }, 1000) 
+ 
+
+        return ()  => clearInterval(interval)
+    }, [counter])
+
+    function calculoSegundos(aCounter, placeValue) {
+        return Math.floor(aCounter / placeValue) % 10
+    }
+    
+    return (
+        <>
+        <SimpleCounter 
+            milDigito = {calculoSegundos(counter, 1000)}
+            cienDigito = {calculoSegundos(counter, 100)}
+            diezDigito = {calculoSegundos(counter, 10)}
+            unDigito = {calculoSegundos(counter, 1)}
+        />
+        </>
+    )
+
+}
 
 //render your react application
 ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
